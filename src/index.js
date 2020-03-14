af071d326e8a6affa713daccc6d4cb976e869327
xaylpq <xaylpq@gmail.com>
2020-03-14 20:49:15 +0300

    react lesson2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = () => {
+    return <h2>Hello world!</h2>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Field = () => {
+    const holder = 'Enter here';
+    const styleField = {
+        width: '250px'
+    }
+
+    return <input
+                style={styleField}
+                type='text' 
+                placeholder={holder}
+                autoComplete=''
+                className='first'
+                htmlFor='' />
+}
+
+const Btn = () => {
+    const res = () => {
+        return 'Click me!'
+    }
+    const logged = true;
+    return <button>{logged ? 'Enter' : res()}</button>
+}
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Field/>
+            <Btn/>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
